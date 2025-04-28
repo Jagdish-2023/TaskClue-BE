a5dc39d3cfbe4aef96a3fe88d6b7aec908d11822
Jagdish-2023 <jagdishjpradhan@gmail.com>
2025-04-28 19:33:58 +0530

    fix: CORS options

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://taskclue.vercel.app/",
+    origin: "*",
     credentials: true,
     optionSuccessStatus: 200,
   })
